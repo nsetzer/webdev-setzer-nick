f6d9dee4ac23fb9ea55ee95bca386261939972d4
Nick Setzer <nsetzer@ccis.neu.edu>
2017-09-15 18:59:21 -0400

    add remaining components + routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,17 @@ import { ProjectComponent } from './components/project/project.component';
 import { LoginComponent } from './components/assignment/user/login/login.component';
 import { ProfileComponent } from './components/assignment/user/profile/profile.component';
 import { RegisterComponent } from './components/assignment/user/register/register.component';
+import { PageEditComponent } from './components/assignment/page/page-edit/page-edit.component';
+import { PageListComponent } from './components/assignment/page/page-list/page-list.component';
+import { PageNewComponent } from './components/assignment/page/page-new/page-new.component';
+import { WebsiteEditComponent } from './components/assignment/website/website-edit/website-edit.component';
+import { WebsiteListComponent } from './components/assignment/website/website-list/website-list.component';
+import { WebsiteNewComponent } from './components/assignment/website/website-new/website-new.component';
+import { WidgetChooseComponent } from './components/assignment/widget/widget-choose/widget-choose.component';
+import { WidgetHeadingComponent } from './components/assignment/widget/widget-heading/widget-heading.component';
+import { WidgetImageComponent } from './components/assignment/widget/widget-image/widget-image.component';
+import { WidgetListComponent } from './components/assignment/widget/widget-list/widget-list.component';
+import { WidgetYoutubeComponent } from './components/assignment/widget/widget-youtube/widget-youtube.component';
 
 @NgModule({
   // Declare components here
@@ -24,7 +35,18 @@ import { RegisterComponent } from './components/assignment/user/register/registe
     ProjectComponent,
     LoginComponent,
     ProfileComponent,
-    RegisterComponent
+    RegisterComponent,
+    PageEditComponent,
+    PageListComponent,
+    PageNewComponent,
+    WebsiteEditComponent,
+    WebsiteListComponent,
+    WebsiteNewComponent,
+    WidgetChooseComponent,
+    WidgetHeadingComponent,
+    WidgetImageComponent,
+    WidgetListComponent,
+    WidgetYoutubeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -20,7 +20,19 @@ import {LoginComponent} from './components/assignment/user/login/login.component
 import {RegisterComponent} from './components/assignment/user/register/register.component';
 import {ProfileComponent} from './components/assignment/user/profile/profile.component';
 
+import {PageEditComponent} from './components/assignment/page/page-edit/page-edit.component';
+import {PageListComponent} from './components/assignment/page/page-list/page-list.component';
+import {PageNewComponent}  from './components/assignment/page/page-new/page-new.component';
 
+import {WebsiteEditComponent} from './components/assignment/website/website-edit/website-edit.component';
+import {WebsiteListComponent} from './components/assignment/website/website-list/website-list.component';
+import {WebsiteNewComponent}  from './components/assignment/website/website-new/website-new.component';
+
+import {WidgetYoutubeComponent} from './components/assignment/widget/widget-youtube/widget-youtube.component';
+import {WidgetListComponent} from './components/assignment/widget/widget-list/widget-list.component';
+import {WidgetImageComponent} from './components/assignment/widget/widget-image/widget-image.component';
+import {WidgetHeadingComponent} from './components/assignment/widget/widget-heading/widget-heading.component';
+import {WidgetChooseComponent} from './components/assignment/widget/widget-choose/widget-choose.component';
 
 const APP_ROUTES: Routes = [
   {path: '', component : HomeComponent},
@@ -30,7 +42,22 @@ const APP_ROUTES: Routes = [
 
   {path: 'view/assignment/user/login',    component: LoginComponent},
   {path: 'view/assignment/user/register', component: RegisterComponent},
-  {path: 'view/assignment/user/profile',  component: ProfileComponent}
+  {path: 'view/assignment/user/profile',  component: ProfileComponent},
+
+  {path: 'view/assignment/page/edit',    component: PageEditComponent},
+  {path: 'view/assignment/page/list',    component: PageListComponent},
+  {path: 'view/assignment/page/new',    component: PageNewComponent},
+
+  {path: 'view/assignment/website/edit',    component: WebsiteEditComponent},
+  {path: 'view/assignment/website/list',    component: WebsiteListComponent},
+  {path: 'view/assignment/website/new',    component: WebsiteNewComponent},
+
+  {path: 'view/assignment/widget/youtube',    component: WidgetYoutubeComponent},
+  {path: 'view/assignment/widget/list',    component: WidgetListComponent},
+  {path: 'view/assignment/widget/image',    component: WidgetImageComponent},
+  {path: 'view/assignment/widget/heading',    component: WidgetHeadingComponent},
+  {path: 'view/assignment/widget/choose',    component: WidgetChooseComponent},
+
 
 ];
 
