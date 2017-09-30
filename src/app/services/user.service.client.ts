ed5dbf4fcba1f2c28dbb88d51cc6190f15cde4c3
Nick Setzer <nsetzer@ccis.neu.edu>
2017-09-30 15:33:27 -0400

    add user service

---
+++ src/app/services/user.service.client.ts
@@ -0,0 +1,75 @@
+
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/Rx';
+import {environment} from '../../environments/environment';
+
+
+@Injectable()
+
+export class UserService {
+
+
+  baseUrl = environment.baseUrl;
+
+  users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ]
+
+  api = {
+    'createUser'   : this.createUser,
+    'findUserById' : this.findUserById
+  };
+
+  constructor(private _http: Http) {
+  }
+
+  createUser(user: any) {
+    user._id = Math.random();
+    this.users.push(user);
+    return user;
+  }
+
+  findUserById(userId: string) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x]._id === userId) {
+        return this.users[x];
+      }
+    }
+  }
+
+  findUserByUsername(username: string) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x].username === username) {
+        return this.users[x];
+      }
+    }
+  }
+
+  updateUser(userId, user) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x]._id === userId) {
+        this.users[x] = user
+      }
+    }
+  }
+
+  deleteUser(userId) {
+    for (let x = 0; x < this.users.length; x++) {
+      if (this.users[x]._id === userId) {
+        this.users.splice(x, 1)
+      }
+    }
+
+  }
+
+
+
+
+}
+
+
+
