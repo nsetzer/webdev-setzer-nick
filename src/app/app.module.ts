54b720525a8ae5c4dc69ecc6a89350aff477f1d6
Nick Setzer <nsetzer@ccis.neu.edu>
2017-10-01 07:53:39 -0400

    implement user login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TestComponent } from './components/test/test.component';
+
 import {Routing} from './app.routing';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
--- src/app/components/assignment/user/login/login.component.ts
+++ src/app/components/assignment/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../services/user.service.client';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css',
               '../../../../../assets/css/style.css' ]
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string
+  password: string
+  invalid_username: boolean
+  invalid_password: boolean
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private _service: UserService) { }
 
   ngOnInit() {
+    this.username = ""
+    this.password = ""
+    this.invalid_username = false
+    this.invalid_password = false
+
   }
 
+  login() {
+
+    var user = this._service.findUserByUsername(this.username)
+
+    if (user) {
+        this.invalid_username = false
+        if (user.password == this.password) {
+            this.router.navigate(["/user/" + user._id]);
+        } else {
+            this.invalid_password = true
+        }
+    } else {
+        this.invalid_username = true
+    }
+
+  }
 }
