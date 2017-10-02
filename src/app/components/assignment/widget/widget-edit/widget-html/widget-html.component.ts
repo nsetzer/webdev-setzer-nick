bceb3833fc4585e765cae3ab7567994168e9522c
Nick Setzer <nsetzer@ccis.neu.edu>
2017-10-02 19:23:25 -0400

    implement remaining widget edits

--- src/app/components/assignment/widget/widget-edit/widget-html/widget-html.component.ts
+++ src/app/components/assignment/widget/widget-edit/widget-html/widget-html.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { WidgetService } from '../../../../../services/widget.service.client';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-widget-html',
   templateUrl: './widget-html.component.html',
-  styleUrls: ['./widget-html.component.css']
+  styleUrls: ['./widget-html.component.css',
+              '../../../../../../assets/css/style.css']
 })
+
 export class WidgetHtmlComponent implements OnInit {
 
-  constructor() { }
+uid : string = "";
+  wid : string = "";
+  pid : string = "";
+  wgid : string = "";
+  widget : {widgetType:"None"};
+
+  private sub: any;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private _service: WidgetService,
+              private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.uid = params['uid'];
+       this.wid = params['wid'];
+       this.pid = params['pid'];
+       this.wgid = params['wgid'];
+
+       this.reload();
+    });
+  }
+
+  reload() {
+    this.widget = this._service.findWidgetById(this.wgid);
+  }
+
+  saveChanges() {
+    this._service.updateWidget(this.wgid, this.widget);
+    let url = "/user/" + this.uid +
+              "/website/" + this.wid +
+              "/page/" + this.pid +
+              "/widget";
+    this.router.navigate([url]);
+  }
+
+  delete() {
+    this._service.deleteWidget(this.wgid);
+    let url = "/user/" + this.uid +
+              "/website/" + this.wid +
+              "/page/" + this.pid +
+              "/widget";
+    this.router.navigate([url]);
   }
 
 }
--- src/app/components/assignment/widget/widget-edit/widget-image/widget-image.component.ts
+++ src/app/components/assignment/widget/widget-edit/widget-image/widget-image.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { WidgetService } from '../../../../../services/widget.service.client';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-widget-image',
   templateUrl: './widget-image.component.html',
-  styleUrls: ['./widget-image.component.css']
+  styleUrls: ['./widget-image.component.css',
+              '../../../../../../assets/css/style.css']
 })
+
 export class WidgetImageComponent implements OnInit {
 
-  constructor() { }
+  uid : string = "";
+  wid : string = "";
+  pid : string = "";
+  wgid : string = "";
+  widget : {widgetType:"None"};
+
+  private sub: any;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private _service: WidgetService,
+              private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.uid = params['uid'];
+       this.wid = params['wid'];
+       this.pid = params['pid'];
+       this.wgid = params['wgid'];
+
+       this.reload();
+    });
+  }
+
+  reload() {
+    this.widget = this._service.findWidgetById(this.wgid);
+  }
+
+  saveChanges() {
+    this._service.updateWidget(this.wgid, this.widget);
+    let url = "/user/" + this.uid +
+              "/website/" + this.wid +
+              "/page/" + this.pid +
+              "/widget";
+    this.router.navigate([url]);
+  }
+
+  delete() {
+    this._service.deleteWidget(this.wgid);
+    let url = "/user/" + this.uid +
+              "/website/" + this.wid +
+              "/page/" + this.pid +
+              "/widget";
+    this.router.navigate([url]);
   }
 
 }
