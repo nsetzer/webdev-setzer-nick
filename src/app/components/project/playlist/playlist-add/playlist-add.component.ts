abff4aff6f853b2c10786961eaef6209c4ebf0e3
Nick Setzer <nsetzer@ccis.neu.edu>
2017-10-06 19:34:15 -0400

    improve playlist routing

--- src/app/components/project/playlist/playlist-add/playlist-add.component.ts
+++ src/app/components/project/playlist/playlist-add/playlist-add.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../../services/user.service.client';
+import { PlaylistService } from '../../../../services/playlist.service.client';
+
 
 @Component({
   selector: 'app-playlist-add',
   templateUrl: './playlist-add.component.html',
   styleUrls: ['./playlist-add.component.css']
 })
+
 export class PlaylistAddComponent implements OnInit {
 
-  constructor() { }
+  uid : string;
+  plid : string;
+  user : any;
+  playlist : any;
+  private sub: any;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private _service: UserService,
+              private _plservice: PlaylistService) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.uid = params['uid'];
+       this.plid = params['plid'];
+       this.reload();
+    });
   }
 
+  reload() {
+    this.user = this._service.findUserById(this.uid)
+
+    this.playlist = this._plservice.findPlaylistById(this.plid);
+
+    if (!this.playlist) {
+        this.playlist = [];
+    }
+
+
+  }
 }
--- src/app/components/project/playlist/playlist-edit/playlist-edit.component.ts
+++ src/app/components/project/playlist/playlist-edit/playlist-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../../services/user.service.client';
+import { PlaylistService } from '../../../../services/playlist.service.client';
+
 
 @Component({
   selector: 'app-playlist-edit',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaylistEditComponent implements OnInit {
 
-  constructor() { }
+  uid : string = "";
+  plid : string = "";
+  pid : string = "";
+  playlist : any;
+  private sub: any;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private _service: UserService,
+              private _plservice: PlaylistService) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.uid = params['uid'];
+       this.plid = params['plid'];
+
+       this.reload();
+    });
+  }
+
+  reload() {
+
+    this.playlist = this._plservice.findPlaylistById(this.plid)
+
+  }
+
+  saveChanges() {
+    this._plservice.updatePlaylist(this.plid, this.playlist);
+    let url = "/project/(project:user/" + this.uid + "/list)"
+    this.router.navigateByUrl(url);
+  }
+
+  delete() {
+    this._plservice.deletePlaylist(this.plid);
+    let url = "/project/(project:user/" + this.uid + "/list)"
+    this.router.navigateByUrl(url);
   }
 
 }
