d92f9a1c19886cbdc6b5b07599a00a09acd9fb51
Nick Setzer <nsetzer@ccis.neu.edu>
2017-10-06 17:11:37 -0400

    add playlist components

---
+++ src/app/components/project/playlist/playlist-search/playlist-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-search',
+  templateUrl: './playlist-search.component.html',
+  styleUrls: ['./playlist-search.component.css']
+})
+export class PlaylistSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/project/playlist/playlist-songs/playlist-songs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-songs',
+  templateUrl: './playlist-songs.component.html',
+  styleUrls: ['./playlist-songs.component.css']
+})
+export class PlaylistSongsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
