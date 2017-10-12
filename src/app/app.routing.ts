016f28e5211eaf155aed38a8e812c3c73d8cba57
Nick Setzer <nsetzer@ccis.neu.edu>
2017-10-11 21:23:47 -0400

    update app routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -50,7 +50,7 @@ const APP_ROUTES: Routes = [
   {path: 'home', component : HomeComponent},
   {path: 'test',       component: TestComponent},
 
-  {path: '',     component: LoginComponent},
+  {path: '',          component: LoginComponent},
   {path: 'login',     component: LoginComponent},
   {path: 'register',  component: RegisterComponent},
   {path: 'user/:uid', component: ProfileComponent},
