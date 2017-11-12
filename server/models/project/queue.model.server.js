522dcdd5955f7a3d78dcaff4fe241cabcb4b87c7
Nick Setzer <nsetzer@ccis.neu.edu>
2017-11-12 15:07:21 -0500

    refactor queue model

--- server/models/project/queue.model.server.js
+++ server/models/project/queue.model.server.js
@@ -1,4 +1,93 @@
 
 module.exports = function(mongoose, UserSchema) {
-    return mongoose.model("QueueModel", UserSchema);
+    model = mongoose.model("QueueModel", UserSchema);
+
+    model.setQueue = setQueue
+    model.getQueue = getQueue
+    model.getQueueHead = getQueueHead
+    model.deleteQueueHead = deleteQueueHead
+
+    async function setQueue(uid,songs) {
+        var queue = {
+            uid: uid,
+            songs: songs
+        };
+
+        await model.update({uid:uid}, queue, {upsert:true})
+
+        return model.find({uid: uid})
+                    .populate('songs')
+    }
+
+    async function getQueue(uid) {
+
+        let queues = await model.find({uid: uid})
+                                .populate('songs')
+
+        // return a default queue
+        if (queues.length==0) {
+            return {uid: uid, songs: []}
+        }
+
+        return queues[0]
+    }
+
+    function _getUrl(song) {
+        if (song.videoId.startsWith("/public")) {
+            return song.videoId;
+        } else {
+            return "/api/youtube/" + song.videoId
+        }
+    }
+
+    async function getQueueHead(uid) {
+        var queues = await model
+            .find({uid:uid})
+            .populate("songs");
+
+        if (queues && queues.length > 0) {
+            let songs = queues[0].songs
+            let song = null;
+            if (songs.length>0) {
+                song = queues[0].songs[0];
+                song.url = _getUrl(song)
+                song.length = queues[0].length;
+            }
+
+            return song;
+        }
+
+        return null;
+    }
+
+    // returns the new head
+    async function deleteQueueHead(uid) {
+        var queues = await model
+            .find({uid:uid})
+            .populate("songs");
+
+        if (queues && queues.length > 0) {
+            let songs = queues[0].songs
+            let song = null;
+            // grab the NEXT song
+            if (songs.length>1) {
+                song = queues[0].songs[1];
+                song.url = _getUrl(song)
+                song.length = queues[0].length-1;
+            }
+            // delete the head.
+            if (songs.length>0) {
+                await model.update(
+                    {uid:uid},
+                    { $pop: { songs: -1 } }
+                );
+            }
+
+            return song;
+        }
+
+        return null;
+    }
+
+    return model;
 }
--- server/project/queue.service.server.js
+++ server/project/queue.service.server.js
@@ -3,106 +3,47 @@ module.exports = function (app, model) {
     var _playlist = require('./playlist.data.server');
     var _message = require('../assignment/message.data.server');
 
-    queues = []
-
     app.put('/api/user/:uid/queue', setSongQueue);
     app.get('/api/user/:uid/queue', getSongQueue);
     app.get('/api/user/:uid/queue/head', getSongQueueHead);
     app.delete('/api/user/:uid/queue/head', deleteSongQueueHead);
 
     function setSongQueue(req, res) {
-        var playlist = req.body;
-        var queue = {
-            uid: req.params.uid,
-            songs: playlist.songs
-        };
-        model.QueueModel
-            .update({uid:req.params.uid}, queue, {upsert:true})
+        model.QueueModel.setQueue(req.params.uid, req.body.songs)
             .then(
-                () => {res.status(200).json(queue)},
-                (err) => {res.status(501).send(_message.Error(err))}
+                (queue) => {
+                    res.status(200).json(queue)
+                },
+                (err) => {
+                    res.status(501).send(_message.Error(err))
+                }
             );
     }
 
     function getSongQueue(req, res) {
 
         model.QueueModel
-            .find({uid: req.params.uid})
-            .populate('songs')
+            .getQueue(req.params.uid)
             .then(
-                (queues) => {
-                    if (queues.length===0) {
-                        // return the default, empty queue, when not found
-                        res.status(200).json({
-                            uid: req.params.uid,
-                            songs: []
-                        });
-                    } else {
-                        // return the queue that was found
-                        res.status(200).json(queues[0])
-                    }
+                (queue) => {
+                    res.status(200).json(queue)
                 },
-                (err) => {res.status(502).send(_message.Error(err))}
+                (err) => {
+                    res.status(502).send(_message.Error(err))
+                }
             );
     }
 
-    function _getUrl(song) {
-        if (song.videoId.startsWith("/public")) {
-            return song.videoId;
-        } else {
-            return "/api/youtube/" + song.videoId
-        }
-    }
-
     async function getSongQueueHead(req, res) {
-
-        var queues = await model.QueueModel
-            .find({uid:req.params.uid})
-            .populate("songs");
-
-        if (queues && queues.length > 0) {
-            let songs = queues[0].songs
-            let song = null;
-            if (songs.length>0) {
-                song = queues[0].songs[0];
-                song.url = _getUrl(song)
-                song.length = queues[0].length;
-            }
-            res.status(200).json(song)
-            return;
-        }
+        song = await model.QueueModel.getQueueHead(req.params.uid)
         // default return a null object
-        res.status(200).json(null)
+        res.status(200).json(song)
     }
 
     async function deleteSongQueueHead(req, res) {
-
-        var queues = await model.QueueModel
-            .find({uid:req.params.uid})
-            .populate("songs");
-
-        if (queues && queues.length > 0) {
-            let songs = queues[0].songs
-            let song = null;
-            // grab the NEXT song
-            if (songs.length>1) {
-                song = queues[0].songs[1];
-                song.url = _getUrl(song)
-                song.length = queues[0].length-1;
-            }
-            // delete the head.
-            if (songs.length>0) {
-                await model.QueueModel.update(
-                    {uid:req.params.uid},
-                    { $pop: { songs: -1 } }
-                );
-            }
-
-            res.status(200).json(song)
-            return;
-        }
+        song = await model.QueueModel.deleteQueueHead(req.params.uid)
         // default return a null object
-        res.status(200).json(null)
+        res.status(200).json(song)
     }
 
 
